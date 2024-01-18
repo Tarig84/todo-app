ed05fd718591f391950b0cf59c0eaf69bc329c15
Tarig <tarigmjob@gmail.com>
2024-01-18 11:00:05 -0800

    Implement the complete and exit day 18-a

--- gui.py
+++ gui.py
@@ -8,8 +8,12 @@ add_button = sg.Button("Add")
 
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45, 10])
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
+
 window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button],
-                                           [list_box, edit_button]], font=("Helvetica", 20))
+                                           [list_box, edit_button, complete_button],
+                                           [exit_button]], font=("Helvetica", 20))
 
 while True:
     event, value = window.read()
@@ -25,6 +29,7 @@ while True:
             todos.append(todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case "Edit":
             todo_to_edit = value['todos'][0]
             new_todo = value['todo'] + "\n"
@@ -33,6 +38,16 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Complete":
+            completed_todo = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(completed_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit":
+            exit()
         case sg.WINDOW_CLOSED:
             break
 window.close()
